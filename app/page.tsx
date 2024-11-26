c22d2165d8fc2ebf3a210482d53b866bdac29571
manhvann <manhvann193@gmail.com>
2024-11-26 18:10:31 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+// This is a server component
+// Add use client for client component
+"use client"
 import Image from "next/image";
 
 export default function Home() {
@@ -81,7 +84,7 @@ export default function Home() {
           Examples
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="flex bg-red-700 items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,56 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+		// Có thể ghi đè các màu css tại đây
+		 
+
+  		colors: {
+			primary: {
+				"50": "#f3f4f6",
+				"100": "#fbe8f0",
+				"200": "#f8d2e1",
+				"300": "#f4adc8",
+				"400": "#ef94b4",
+				"500": "#e15381",
+				"600": "#cf335d",
+				"700": "#b32346",
+				"800": "#94203b",
+				"900": "#7c1f3b"
+			},
+			secondary: {
+				"50": "#f0f8ff",
+				"100": "#e0f0fe",
+				"200": "#bae2fd",
+				"300": "#7ccbfd",
+				"400": "#37b1f9",
+				"500": "#0d97ea",
+				"600": "#016fb9",
+				"700": "#025fa2",
+				"800": "#065186",
+				"900": "#0c446e"
+			},
+
+			//Base Color 
+			black: "#1f1f1f",
+			white: "#ffffff",
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
